34833b2c08ea64cb8108b845b1f4d873fca387ca
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-18 20:59:49 +0800

    run package msbuild

---
+++ @export/index.d.ts
@@ -0,0 +1,11 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=Daisy
+
+declare namespace Daisy {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
