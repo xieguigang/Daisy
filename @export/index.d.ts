26f1af0f2614d0503c3c23045a1cf64060627b7b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-18 21:31:21 +0800

    run package msbuild

--- @export/index.d.ts
+++ @export/index.d.ts
@@ -8,4 +8,17 @@ declare namespace Daisy {
       */
       function onLoad(): object;
    }
+   /**
+     * @param libtype default value Is ``[1, -1]``.
+     * @param ms1ppm default value Is ``15``.
+     * @param export_dir default value Is ``./``.
+   */
+   function make_annotation(files: any, peakfile: any, libtype?: any, ms1ppm?: any, export_dir?: any): object;
+   /**
+     * @param libtype default value Is ``[1, -1]``.
+   */
+   function metadna(peaks_ms2: any, libtype?: any): object;
+   /**
+   */
+   function read_rawfile(files: any): object;
 }
