8b8f161e26ba66fa567182621bae01529aa9b688
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-23 12:21:34 +0800

    build search index for refmet

--- build/build.R
+++ build/build.R
@@ -1,8 +1,17 @@
+require(Darwinism);
 require(Daisy);
+
+imports "memory_query" from "Darwinism";
 
 # script for build local reference database
 let mspfiles   = list.files("./MONA/", pattern = "*.msp");
 let lib_export = "../data/MoNA/";
+let refmet     = "./refmet.csv"
+|> read.csv(row.names = NULL, check.names = FALSE)
+|> memory_query::load()
+|> hashindex(["refmet_name","pubchem_cid"])
+|> valueindex(exactmass = "number")
+;
 let lib.pos    = spectrumTree::new(file.path(lib_export, "lib.pos.pack"), type = "Pack");
 let lib.neg    = spectrumTree::new(file.path(lib_export, "lib.neg.pack"), type = "Pack");
 
