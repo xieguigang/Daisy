0c6bc2af41a9b7cde45f3d6f7d53818754ff02dc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-24 21:12:54 +0800

    read raw data file

--- R/parallel.R
+++ R/parallel.R
@@ -1,3 +1,5 @@
 const dasy_task = function(file, args) {
-
+    args$rawdata = read_rawfile(file, cache_enable = TRUE);
+    args$filename = basename(file);
+    
 }
--- R/processing/rawfile.R
+++ R/processing/rawfile.R
@@ -1,8 +1,18 @@
-const read_rawfile = function(files) {
-    let read(file) = open.mzpack(file) |> ms2_peaks();
+const read_rawfile = function(file, cache_enable = TRUE) {
+    let cache_file = file.path(dirname(file), `${basename(file)}.cache`);
 
-    files 
-    |> lapply(path -> read(path))
-    |> unlist()
-    ;
+    if (file.ext(file) == "peakms2") {
+        # spectrum in ms2, which already been processed
+        mzweb::read.cache(file);
+    } else {
+        if (cache_enable && file.exists(cache_file)) {
+            mzweb::read.cache(cache_file);
+        } else {
+            # read rawdata file by a unify method
+            let ms2data = open.mzpack(file) |> ms2_peaks();
+            # cache file could be read from next run
+            mzweb::write.cache(ms2data, cache_file);
+            ms2data;
+        }
+    }
 }
