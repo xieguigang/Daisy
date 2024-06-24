b715b28de9a91b8140dca4c8873abc8741dc1ab1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-24 21:17:26 +0800

    Update parallel.R

--- R/parallel.R
+++ R/parallel.R
@@ -1,5 +1,8 @@
 const dasy_task = function(file, args) {
     args$rawdata = read_rawfile(file, cache_enable = TRUE);
     args$filename = basename(file);
+    args$peaks = read.xcms_peaks(args$peakfile,
+        tsv = file.ext(args$peakfile) == "txt",
+        general_method = TRUE);
     
 }
