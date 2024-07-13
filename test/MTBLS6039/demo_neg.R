38a8bc60c0051329c5034e5769a7d51f6ac61b08
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-13 15:09:22 +0800

    config demo script

--- test/MTBLS6039/demo_neg.R
+++ test/MTBLS6039/demo_neg.R
@@ -1,10 +1,10 @@
 require(Daisy);
 
 Daisy::make_annotation(
-    files = file.path(@dir, "demo_data"), 
-    peakfile = file.path(@dir, "demo_data/peaktable.csv"), 
+    files = file.path(@dir, "neg"), 
+    peakfile = file.path(@dir, "neg.csv"), 
     libtype = 1, 
     ms1ppm = 15, 
-    export_dir = file.path(@dir, "result_outputs"),
+    export_dir = file.path(@dir, "result_outputs","neg"),
     debug = TRUE
 );
--- test/MTBLS6039/demo_pos.R
+++ test/MTBLS6039/demo_pos.R
@@ -1,10 +1,10 @@
 require(Daisy);
 
 Daisy::make_annotation(
-    files = file.path(@dir, "demo_data"), 
-    peakfile = file.path(@dir, "demo_data/peaktable.csv"), 
+    files = file.path(@dir, "pos"), 
+    peakfile = file.path(@dir, "pos.csv"), 
     libtype = 1, 
     ms1ppm = 15, 
-    export_dir = file.path(@dir, "result_outputs"),
+    export_dir = file.path(@dir, "result_outputs","pos"),
     debug = TRUE
 );
