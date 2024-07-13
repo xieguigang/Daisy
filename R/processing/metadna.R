23cc10717cccbe2e937c4c302e0ca0eb85df8665
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-13 12:43:06 +0800

    Update metadna.R

--- R/processing/metadna.R
+++ R/processing/metadna.R
@@ -2,6 +2,8 @@ imports "metadna" from "mzDIA";
 imports "mzweb" from "mzkit";
 imports ["assembly", "data", "math"] from "mzkit";
 
+#' run metadna algorithm
+#' 
 const call_metadna = function(peaks_ms2, libtype = [1,-1], ms1ppm = 15, output = "./") {
     options(verbose = TRUE);
 
@@ -33,4 +35,47 @@ const call_metadna = function(peaks_ms2, libtype = [1,-1], ms1ppm = 15, output =
     ;
 
     write.csv(unique_result, file = `${output}/metaDNA.csv`);
+}
+
+#' make peak alignment of the metadna result with peaktable
+#' 
+const peak_alignment = function(metadna, peaktable, mzdiff = 0.01, rt_win = 15) {
+    let alignments = list();
+    let hit_result = NULL;
+
+    for(let hit in tqdm(as.list(metadna, byrow = TRUE))) {
+        let peaks = peaktable |> find_xcms_ionPeaks(mz  = hit$mz, rt = hit$rt,
+            mzdiff = mzdiff,
+            rt_win = rt_win); 
+
+        # no ion peaks matched result will be ignores at here
+        for(peak in peaks) {
+            hit_result = as.list(hit);
+            hit_result$xcms_id = [peak]::ID;
+            hit_result$mz = [peak]::mz;
+            hit$result$rt = [peak]::rt;
+            alignments[[`${id}-${hit$query_id}`]] = hit_result;
+        }
+    }
+
+    data.frame(
+        xcms_id = alignments@xcms_id,
+        mz = alignments@mz,
+        rt = alignments@rt,
+        intensity = alignments@intensity,
+        KEGG = alignments@KEGGId,
+        exactMass = alignments@exactMass,
+        formula = alignments@formula,
+        name = alignments@name,
+        precursorType = alignments@precursorType,
+        mzCalc = alignments@mzCalc,
+        ppm = alignments@ppm,
+        forward	= alignments@forward,
+        reverse = alignments@reverse,
+        jaccard = alignments@jaccard,
+        entropy = alignments@entropy,
+        pvalue	= alignments@pvalue,
+        alignment = alignments@alignment,
+        row.names = alignments@xcms_id
+    );
 }
