204678e4021362736039d8643e53830316ad6505
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-14 16:07:27 +0800

    test success

--- test/MTBLS6039/union_test.R
+++ test/MTBLS6039/union_test.R
@@ -1,6 +1,7 @@
 require(Daisy);
 
-let posneg = Daisy::union_posneg(pos = file.path(@dir, "result_outputs/pos/annotation_result.csv"), neg = file.path(@dir, "result_outputs/neg/annotation_result.csv"))
+let posneg = Daisy::union_posneg(pos = file.path(@dir, "result_outputs/pos/annotation_result.csv"), 
+neg = file.path(@dir, "result_outputs/neg/annotation_result.csv"));
 
 write.csv(posneg, file = file.path(@dir, "result_outputs/annotation.csv" ), row.names = FALSE);
 
