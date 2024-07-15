1f9616251eae31c441e0ad1b2d3e258eb94a8a93
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-15 21:42:46 +0800

    search ms1?

--- R/processing/formula.R
+++ R/processing/formula.R
@@ -1,3 +1,9 @@
-const formula_annotation = function() {
+#'
+#' @param peaks the peaktable object, a mzkit peakSet object model
+#' 
+const formula_annotation = function(peaks, 
+                                    ms1ppm = 10, 
+                                    profiles = list(C = [1,100],H = [3,200],O=[0,50],N=[0,50],S=[0,50],P=[0,50]), 
+                                    mass_range = [50,1200]) {
     
 }
