d1f789b9a9a14111649c491d8293a7fa0a1dab06
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-08 14:40:37 +0800

    Update mona.R

--- R/library/mona.R
+++ R/library/mona.R
@@ -2,6 +2,15 @@
 #' 
 #' @param mspfile the filepath to the msp library file
 #' 
+#' @return a cluster tree of the reference spectrum for make spectrum 
+#'   similarity search.
+#' 
 const gcms_mona_msp = function(mspfile) {
+    imports "massbank" from "mzkit";
+    imports "annotation" from "mzkit";
 
+    let mona = massbank::read.MoNA(mspfile, is_gcms =TRUE,lazy=FALSE);
+    let library = annotation::library_from_mona(mona = mona);
+
+    library;
 }
