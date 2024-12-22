d47f74c297cc1116970efafa1a261b30af56551e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-22 13:17:21 +0800

    Update gcms_tof.R

--- R/gcms_tof.R
+++ R/gcms_tof.R
@@ -6,7 +6,7 @@ const gcms_tof_annotation = function(rawdir, peaktable,
     libtype   = [1,-1], 
     outputdir = "./", 
     ppm_cutoff = 20,
-    lib_files = NULL, # system.file("data/MoNA-export-GC-MS_Spectra.msp", package = "Daisy"), 
+    lib_files = NULL,  
     n_threads = 8,
     debug = FALSE) {
 
@@ -25,11 +25,7 @@ const gcms_tof_annotation = function(rawdir, peaktable,
 
     if (debug || (n_threads == 1)) {
         for(let file in rawfiles) {
-            let checkfile = file.path(outputdir,`${basename(file)}.csv`);
-
-            if (!file.exists(checkfile)) {
-                file |> __gcms_annotation(peaktable, work_pars);
-            }            
+            Daisy::__gcms_file_annotation(file,peaktable,work_pars);      
         }
     } else {
         # run in parallel for production
@@ -40,15 +36,7 @@ const gcms_tof_annotation = function(rawdir, peaktable,
 
             require(Daisy);
 
-            let outputdir = work_pars$outputdir;
-            let checkfile = file.path(outputdir,`${basename(filepath)}.csv`);
-
-            if (!file.exists(checkfile)) {
-                # processing a single rawdata file
-                Daisy::__gcms_annotation(filepath, peaktable, 
-                    libs =  __load_gcms_libs(work_pars), 
-                    argv = work_pars);
-            }
+            Daisy::__gcms_file_annotation(filepath,peaktable,work_pars);
         }
     }
 
@@ -58,7 +46,47 @@ const gcms_tof_annotation = function(rawdir, peaktable,
     result |> make_msms_plot(file.path(outputdir,"plotMs"));
 }
 
-const __gcms_annotation = function(rawfile, peaktable, libs, argv) {
+const __gcms_file_annotation = function(filepath, peaktable,work_pars) {
+    let outputdir = work_pars$outputdir;
+    let checkfile = file.path(outputdir,`${basename(filepath)}.csv`);
+
+    if (!file.exists(checkfile)) {
+        if (is.null(work_pars$libfiles)) {
+            # default internal
+            let msp = system.file("data/MoNA-export-GC-MS_Spectra.msp", package = "Daisy");
+
+            Daisy::__gcms_annotation(filepath, peaktable, 
+                libname = basename(msp),
+                libs = Daisy::gcms_mona_msp(msp, libtype = work_pars$libtype), 
+                argv = work_pars);
+            Daisy::__gcms_annotation(filepath, peaktable, 
+                libname = "biocad_registry",
+                libs = Daisy::local_gcms_lib(), 
+                argv = work_pars);
+
+        } else {
+            # load external msp file
+            for(let libfile in work_pars$libfiles) {
+                # processing a single rawdata file
+                Daisy::__gcms_annotation(filepath, peaktable, 
+                    libname = basename(libfile),
+                    libs = Daisy::gcms_mona_msp(libfile, libtype = work_pars$libtype), 
+                    argv = work_pars);
+            }
+        }
+
+        # merge library result
+        let tempfiles = file.path(work_pars$outputdir,"tmp",basename(filepath)) |> list.files(pattern = "*.csv");
+        let tempdata = as.list(tempfiles, basename(tempfiles)) |> lapply(path -> read.csv(path, row.names = NULL, check.names = FALSE));
+        let union = bind_rows(tempdata);
+
+        write.csv(union, file = checkfile, row.names = FALSE);
+    }
+}
+
+#' run single gcms annotation
+#' 
+const __gcms_annotation = function(rawfile, peaktable, libname, libs, argv) {
     let outputdir = argv$outputdir;
     let tmp = file.path(outputdir,"tmp");
     let top = as.integer(argv$top || 9);
@@ -96,6 +124,6 @@ const __gcms_annotation = function(rawfile, peaktable, libs, argv) {
     result <- cbind(annotation, scores);
 
     write.csv(result, 
-        file = file.path(outputdir,`${basename(rawfile)}.csv`), 
+        file = file.path(tmp, basename(rawfile), `${libname}.csv`), 
         row.names = FALSE);
 }
