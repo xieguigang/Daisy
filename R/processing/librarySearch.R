ffbef74611f47a1df3b93cc161267fa739d535ee
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-13 23:16:58 +0800

    Update librarySearch.R

--- R/processing/librarySearch.R
+++ R/processing/librarySearch.R
@@ -1,4 +1,11 @@
 const call_librarysearch = function(peaks_ms2, libfiles = NULL, libtype = [1,-1], ms1ppm = 15, output = "./") {
     let [libs, metadb ] = __load_lcms_libs(list(libfiles = libfiles, libtype = libtype));
+    let result = lapply(peaks_ms2, function(sample) {
+        libs 
+        |> query(sample, top_hits = 9) 
+        |> as.annotation_result(metadb)
+        ;
+    });
+
     
 }
