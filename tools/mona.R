ca7f6d2fc8c516cfafb9eed929020004255816cc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-13 17:19:45 +0800

    Update mona.R

--- tools/mona.R
+++ tools/mona.R
@@ -2,11 +2,12 @@ require(Daisy);
 
 let mona_msp_repo = ?"--mona" || stop("A directory path that contains multiple mona database msp file must be provided!");
 let lib_output = ?"--output" || normalizePath(file.path(mona_msp_repo,"libs"));
+let list_repo_files = list.files(mona_msp_repo, pattern = "*.msp");
 
 print("Start to build mona reference library:");
 print(mona_msp_repo);
 print(`  -> ${lib_output}`);
 print("library files:");
-print(basename(list.files(mona_msp_repo, pattern = "*.msp")));
+print(basename(list_repo_files));
 
-Daisy::build_mona_lcms(mona_msp_repo, lib_output);
+Daisy::build_mona_lcms(list_repo_files, lib_output);
