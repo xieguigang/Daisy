07ac6507318e09b5dfee4acaf20a265993bc9b58
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-13 16:45:25 +0800

    load mona data and write metabolite annotations

--- R/library/tools/build_mona_lcms.R
+++ R/library/tools/build_mona_lcms.R
@@ -1,8 +1,25 @@
+imports "massbank" from "mzkit";
+imports "spectrumTree" from "mzkit";
+imports "annotation" from "mzkit";
+
 #' build lcms reference library for MoNA LC-MS dataset
 #' 
 #' @param repo a local directory path that contains multiple msp data files that 
 #'    could be download from the MoNA database. 
 #' 
 const build_mona_lcms = function(repo, libdir = "./MoNA") {
-    
+    let spectra = read.MoNA(repo, lazy = FALSE);
+    let metabolites = extract_mona_metabolites(mona = spectra);
+    let metadata = open_repository(file.path(libdir,"metadata.dat"), mode = "write");
+    let libpos = spectrumTree::new(file.path(libdir,"lib.pos.pack"), "Pack");
+    let libneg = spectrumTree::new(file.path(libdir,"lib.neg.pack"), "Pack");
+
+    attr(metabolites, "mapping")
+    |> JSON::json_encode()
+    |> writeLines(con = file.path(libdir,"mapping.json"))
+    ;
+
+    write_metadata(metadata, meta = metabolites);
+
+    close(metadata);
 }
