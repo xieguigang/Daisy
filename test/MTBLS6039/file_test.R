31dc4e69f9217e374c03176f149d1178f907c96b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-14 16:10:55 +0800

    Update file_test.R

--- test/MTBLS6039/file_test.R
+++ test/MTBLS6039/file_test.R
@@ -7,6 +7,6 @@ dasy_task(file = "./pos/L1.PeakMs2", args = list(
         peakfile   = "./pos.csv",
         ms1_da     = 0.1, 
         rt_winsize = 10, 
-        library_dir = "",
+        library_dir = "E:\biodeep\mona\libs",
         libtype    = 1, 
         ms1ppm     = 15));
