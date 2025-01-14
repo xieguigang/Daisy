2d9e8080c898a6bb717204564239b0c238c29216
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-14 19:01:04 +0800

    fix of the missing module imports

--- R/includes.R
+++ R/includes.R
@@ -5,3 +5,4 @@ imports ["assembly", "massbank"] from "mzkit";
 imports "spectrumTree" from "mzkit";
 imports "mzweb" from "mzkit";
 imports "visual" from "mzplot";
+imports ["formula","math"] from "mzkit";
