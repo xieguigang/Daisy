933b2e856f87ff800ca3ec01dcfd6198f7f6c798
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-14 16:01:58 +0800

    Update parallel.R

--- R/parallel.R
+++ R/parallel.R
@@ -4,7 +4,14 @@
 #'    be in file format of mzkit supported mzXML/mzML/mzPack. 
 #' @param args the workflow arguments for run this annotation.
 #' 
-const dasy_task = function(file, args) {
+const dasy_task = function(file, args = list(
+        export_dir = "./", 
+        peakfile   = "./peaksdata.csv",
+        ms1_da     = 0.1, 
+        rt_winsize = 10, 
+        libtype    = 1, 
+        ms1ppm     = 15)) {
+
     # get a collection of the mzkit peakms2 clr object
     # the precursor of the ms2 spectrum has not changed to ms1 peaks ion
     args$rawdata = read_rawfile(file, cache_enable = TRUE);
@@ -17,7 +24,8 @@ const dasy_task = function(file, args) {
 
     let metadna_exports = file.path(args$export_dir, "metadna");
     let library_exports = file.path(args$export_dir, "libsearch");
-    let dda_result = NULL;
+    let ms1_da = args$ms1_da || 0.1;
+    let rt_winsize = args$rt_winsize || 10;
 
     # run reference library search
     call_librarysearch(
@@ -36,8 +44,19 @@ const dasy_task = function(file, args) {
 
     let metadna_result = `${metadna_exports}/metaDNA.csv` 
     |> read.csv(row.names = NULL, check.names = FALSE) 
+    # the annotation result dataframe required of mz and rt field
     |> peak_alignment(args$peaks, 
-        mzdiff = 0.01, rt_win = 15);
+        mzdiff = ms1_da, 
+        rt_win = rt_winsize)
+    ;
+    
+    let dda_result = `library_exports/libsdata.csv`
+    |> read.csv(row.names = NULL, check.names = FALSE) 
+    # the annotation result dataframe required of mz and rt field
+    |> peak_alignment(args$peaks, 
+        mzdiff = ms1_da, 
+        rt_win = rt_winsize)
+    ;
 
     let result = rbind(dda_result, metadna_result);
 
