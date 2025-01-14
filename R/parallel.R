792b4af446014139e822d4d82ff1e4145ddd78cf
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-01-14 16:05:39 +0800

    config cache

--- R/parallel.R
+++ R/parallel.R
@@ -12,9 +12,11 @@ const dasy_task = function(file, args = list(
         libtype    = 1, 
         ms1ppm     = 15)) {
 
+    let opt_cache_enable = TRUE;
+
     # get a collection of the mzkit peakms2 clr object
     # the precursor of the ms2 spectrum has not changed to ms1 peaks ion
-    args$rawdata = read_rawfile(file, cache_enable = TRUE);
+    args$rawdata = read_rawfile(file, cache_enable = opt_cache_enable);
     args$filename = basename(file);
     args$export_dir = file.path(args$export_dir, args$filename);
     # get peak table data
@@ -24,25 +26,31 @@ const dasy_task = function(file, args = list(
 
     let metadna_exports = file.path(args$export_dir, "metadna");
     let library_exports = file.path(args$export_dir, "libsearch");
+    let dia_output = `${metadna_exports}/metaDNA.csv`; 
+    let dda_output = `${library_exports}/libsdata.csv`;
     let ms1_da = args$ms1_da || 0.1;
     let rt_winsize = args$rt_winsize || 10;
 
-    # run reference library search
-    call_librarysearch(
-        peaks_ms2 = args$rawdata, 
-        libfiles = NULL, 
-        libtype = args$libtype, 
-        ms1ppm = args$ms1ppm, 
-        output = library_exports);
+    if (length(readLines(dda_output)) == 0 || !opt_cache_enable) {
+        # run reference library search
+        call_librarysearch(
+            peaks_ms2 = args$rawdata, 
+            libfiles = NULL, 
+            libtype = args$libtype, 
+            ms1ppm = args$ms1ppm, 
+            output = library_exports);
+    }
 
-    # run metadna at last
-    call_metadna(
-        peaks_ms2 = args$rawdata, 
-        libtype = args$libtype, 
-        ms1ppm = args$ms1ppm, 
-        output = metadna_exports);
+    if (length(readLines(dia_output)) == 0 || !opt_cache_enable) {
+        # run metadna at last
+        call_metadna(
+            peaks_ms2 = args$rawdata, 
+            libtype = args$libtype, 
+            ms1ppm = args$ms1ppm, 
+            output = metadna_exports);
+    }
 
-    let metadna_result = `${metadna_exports}/metaDNA.csv` 
+    let metadna_result = dia_output
     |> read.csv(row.names = NULL, check.names = FALSE) 
     # the annotation result dataframe required of mz and rt field
     |> peak_alignment(args$peaks, 
@@ -50,7 +58,7 @@ const dasy_task = function(file, args = list(
         rt_win = rt_winsize)
     ;
     
-    let dda_result = `library_exports/libsdata.csv`
+    let dda_result = dda_output
     |> read.csv(row.names = NULL, check.names = FALSE) 
     # the annotation result dataframe required of mz and rt field
     |> peak_alignment(args$peaks, 
