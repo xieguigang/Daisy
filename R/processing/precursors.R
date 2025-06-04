19f544383495f05a5adffa60052f8451574fce32
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-04 11:20:34 +0800

    Update precursors.R

--- R/processing/precursors.R
+++ R/processing/precursors.R
@@ -2,7 +2,7 @@
 #' 
 let get_adducts = function(ionMode = [1,-1]) {
 	if (.Internal::first(ionMode) == 1) {
-		["[M]+", "[M+H]+", "[M+H-H2O]+", "[M+NH4]+"];
+		["[M]+", "[M+H]+", "[M+H-H2O]+", "[M+NH4]+", "[2M+H]+", "[M-Cl]+", "[M+Na]+", "[M+K]+"];
 	} else {
 		["[M]-", "[M-H]-", "[M-H2O-H]-", "[M+HCOO]-", "[M+CH3COO]-"];
 	}
