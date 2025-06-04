5b0af566b8d60a1816ac46f751e1b642a19b1198
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-04 11:33:11 +0800

    make parallel

--- R/xcms/deconv_peaks.R
+++ R/xcms/deconv_peaks.R
@@ -3,16 +3,37 @@
 const deconv_peaks = function(rawfiles, workdir = "./", 
                               max_rtwin = 15, 
                               docker = NULL, 
+                              n_threads = 8,
                               call_xcms = TRUE) {
 
     let xcms_out = file.path(workdir,"xcms");
-
+    
     if (as.logical(call_xcms)) {
-        # findPeaks via xcms package
-        Daisy::xcms_findPeaks(
-            rawfiles, 
-            workdir = xcms_out, 
-            docker = docker);
+        let batch = rawfiles |> split(size = (length(rawfiles) / n_threads) - 1);
+        let docker_id = docker;
+
+        # run in parallel for production
+        Parallel::parallel(raw_file = batch, n_threads = n_threads, 
+                ignoreError = FALSE, 
+                debug = FALSE,
+                log_tmp = `${xcms_out}/.local_debug/`) {
+
+            require(Daisy);
+
+            let filepath <- unlist(raw_file);
+
+            # view verbose debug echo 
+            print("get batch of rawdata files for processing:");
+            print(` -> ${basename(filepath)}`);            
+
+            # processing a batch list of rawdata file
+            # findPeaks via xcms package
+            Daisy::xcms_findPeaks(
+                filepath, 
+                workdir = unlist(xcms_out), 
+                docker = unlist(docker_id)
+            );
+        }
     }
 
     xcms_out |> __peak_alignment(max_rtwin, workdir);
