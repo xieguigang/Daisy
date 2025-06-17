9bd462b0d85a66a8cb99299bfe999b62b26a5bac
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-17 21:27:53 +0800

    Update includes.R

--- R/includes.R
+++ R/includes.R
@@ -4,6 +4,7 @@ require(GCModeller);
 imports ["assembly", "massbank"] from "mzkit";
 imports "spectrumTree" from "mzkit";
 imports "mzweb" from "mzkit";
+imports "metadb" from "mzkit";
 imports "visual" from "mzplot";
 imports ["formula","math"] from "mzkit";
 imports "Parallel" from "snowFall";
