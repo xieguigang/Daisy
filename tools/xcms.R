c4a738ba30dc8ba3f090d093a50c6cbfacefdfde
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-27 01:00:00 +0800

    Update xcms.R

--- tools/xcms.R
+++ tools/xcms.R
@@ -8,7 +8,7 @@ print("processing of the raw data files:");
 print(rawfiles);
 
 Daisy::deconv_peaks(rawfiles, workdir = output_dir, 
-                              max_rtwin = 15, 
-                              docker = NULL, 
-                              n_threads = 8,
-                              call_xcms = TRUE);
+                            max_rtwin = 15, 
+                            docker = NULL, 
+                            n_threads = 8,
+                            call_xcms = TRUE);
